9d004f7183d88e17c816e1b8632f680d09d8eeaa
QuenProgrammer <adamsadam3667@gmail.com>
2025-08-03 20:36:07 -0700

    new

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "cashbook-979b7", appId: "1:575903547165:web:c7b809df944630a7716bf4", storageBucket: "cashbook-979b7.firebasestorage.app", apiKey: "AIzaSyAw6Nor69h7X2uHI1EfujHHwg-717KIOOA", authDomain: "cashbook-979b7.firebaseapp.com", messagingSenderId: "575903547165", measurementId: "G-E62D6DZXR3" })), provideFirestore(() => getFirestore())
   ]
 };
